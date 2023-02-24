d5bba5d9b8bd8ca0c67394b8a9e85c1f78b595de
felix9909 <long99090704@gmail.com>
2023-02-24 12:35:05 +0800

    初始化 springboot project

---
+++ src/test/java/com/felix/springbootshopping/SpringbootShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.springbootshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
